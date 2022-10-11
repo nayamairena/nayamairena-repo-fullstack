b259d60d6ed45ca0bd3322bd51117f0f7ae8fc7d
Naya <44099724+nayamairena@users.noreply.github.com>
2022-10-10 23:08:10 -0700

    add comments to form.

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -5,8 +5,10 @@
  */
 /** Exercise 03 - Form **/
 
+//Get the form tag from the html file.
 let form = document.querySelector('form');
 
+//Create all the html tags that will be necessary.
 let section1 = document.createElement('section');
 let nameLabel = document.createElement('label');
 let nameInput = document.createElement('input');
@@ -23,40 +25,51 @@ let section5 = document.createElement('section');
 let submitButton = document.createElement('button');
 let resetButton = document.createElement('button');
 
+//First section for the name input. Using bootstrap class names.
 section1.className = 'mb-3';
 nameLabel.textContent = 'Name*';
 nameLabel.className = 'form-label';
 nameInput.type = 'text';
+//A name must be entered.
 nameInput.required = 'true';
 nameInput.className = 'form-control';
+//Attach label to the input for accessibility.
 nameInput.id = 'name';
 nameLabel.htmlFor = 'name';
+//Append the name label and input to the section.
 section1.append(nameLabel);
 section1.append(nameInput);
+//Append the section to the form.
 form.append(section1);
 
+//Second section for email input.
 section2.className = 'mb-3';
 emailLabel.textContent = 'Email*';
 emailLabel.className = 'form-label';
 emailInput.type = 'email';
+//Email must be entered.
 emailInput.required = 'true';
 emailInput.className = 'form-control';
+//Attach label to the input for accessibility.
 emailInput.id = 'email';
 emailLabel.htmlFor = 'email';
 section2.append(emailLabel);
 section2.append(emailInput);
 form.append(section2);
 
+//Third section to submit a message.
 section3.className = 'mb-3';
 messageLabel.textContent = 'Submit your message:';
 messageLabel.className = 'form-label';
 messageInput.className = 'form-control';
+//Attach label to the input for accessibility.
 messageInput.id = 'message';
 messageLabel.htmlFor = 'message';
 section3.append(messageLabel);
 section3.append(messageInput);
 form.append(section3);
 
+//Fourth section for checkbox.
 section4.className = 'mb-3';
 checkboxLabel.className = 'form-check-label ms-2';
 checkboxLabel.textContent = 'Sign up for the newsletter';
@@ -68,6 +81,7 @@ section4.append(checkboxInput);
 section4.append(checkboxLabel);
 form.append(section4);
 
+//Fifth section for buttons. Using Bootstrap grid system.
 section5.className = 'mb-3 row';
 submitButton.className = 'ms-3 col btn btn-primary';
 submitButton.type = 'submit';
@@ -79,15 +93,21 @@ section5.append(submitButton);
 section5.append(resetButton);
 form.append(section5);
 
+//EventListener when the submit button is pressed.
 form.addEventListener('submit', handleSubmit);
+
+//Function to handle when submit button is pressed.
 function handleSubmit(event) {
   event.preventDefault();
   console.log('========= Form Submission =========');
   console.log('Name: ' + nameInput.value);
   console.log('Email: ' + emailInput.value);
+  //Check if feedback input is empty to display alternative message.
   if (!messageInput.value) console.log('No feedback was submitted.');
   else console.log('Feedback: ' + messageInput.value);
+  //Check if checkbox is checked or unchecked.
   if (checkboxInput.checked == true)
     console.log('Newsletter: Yes, I would like to join the newsletter.');
-  if (checkboxInput.checked == false) console.log('Newsletter: No, thank you.');
+  else if (checkboxInput.checked == false)
+    console.log('Newsletter: No, thank you.');
 }
