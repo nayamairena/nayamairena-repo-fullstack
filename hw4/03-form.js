3c67d89acc536cfb061e13986936c683ad9fa82f
Naya <44099724+nayamairena@users.noreply.github.com>
2022-11-05 20:35:54 -0700

    03-forms done.

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -5,12 +5,20 @@ const port = process.env.PORT || 5001;
 // Use middleware static() to serve all static files in the given folder
 app.use(express.static('public'));
 
-// Use middleware urlencoded() to parse an incoming request with a urlencoded payload and return an objectß
+// Use middleware urlencoded() to parse an incoming request with a urlencoded payload and return an object
 app.use(express.urlencoded({ extended: false }));
 
 // POST request
-app.post('', (req, res) => {
+app.post('/submit', (req, res) => {
   // Add your code here
+  res.status(200);
+  res.set({ 'Content-Type': 'text/html' });
+  res.write(`Name: ${req.body.Name} <br>`);
+  res.write(`Email: ${req.body.Email} <br>`);
+  res.write(`Comments: ${req.body.Comments} <br>`);
+  if (req.body.check) res.write(`Newsletter: Yes, sign me up.`);
+  else res.write(`Newsletter: No, thank you.`);
+  res.end();
 });
 
 app.listen(port, () => {
